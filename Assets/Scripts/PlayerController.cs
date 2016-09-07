33860d9a18a5a1ce9d8b56427d807bc9adde4a51
Matt Rosanio <mmrosani@syr.edu>
2016-09-07 14:39:59 -0700

    Add basic player controls and a few platforms

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[HideInInspector] public bool jump;
+
+	public float moveForce = 365f;
+	public float jumpForce = 1000f;
+	public float maxSpeed = 5f;
+	public Transform groundCheck;
+
+	private bool grounded = false;
+	private Rigidbody2D rb2d;
+
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if(Input.GetButtonDown("Jump") && grounded) {
+			jump = true;
+		}
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxis("Horizontal");
+
+		if(h*rb2d.velocity.x < maxSpeed) {
+			rb2d.AddForce(Vector2.right * h * moveForce);
+		}
+
+		if(Mathf.Abs(rb2d.velocity.x) > maxSpeed) {
+			rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x)*maxSpeed, rb2d.velocity.y);
+		}
+
+		if(jump) {
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+
+		if(rb2d.velocity.y > 0) {
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Ground"), true);
+		} else {
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Ground"), false);
+		}
+	}
+}
